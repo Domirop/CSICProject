5978206c7e4b4745d04c845832911962e0dc4b51
dd8888 <doubled000000@gmail.com>
2020-05-05 13:06:12 +0000

    Cambios

--- CSIC/src/csic/CSIC.java
+++ CSIC/src/csic/CSIC.java
@@ -8,14 +8,8 @@ package csic;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Hashtable;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
-
 
 /**
  *
@@ -28,7 +22,7 @@ public class CSIC {
      */
     public static void main(String[] args) {
         try {
-            String string = Files.lines(Paths.get("C:\\Users\\domit\\Desktop\\01_lsw3_2R3S5R12R_1_DP4J.log"))
+            String string = Files.lines(Paths.get("/Users/daviddiaz/Downloads/out/01_lsw3_2R3S5R12R_1_DP4J.log"))
                     .filter(s -> s.contains("Isotropic"))
                     .findFirst()
                     .get();
@@ -39,16 +33,21 @@ public class CSIC {
                 System.out.println(t);
             
             });*/
-            char[] prueba = string.toCharArray();
-            for (int i = 0; i < prueba.length; i++) {
-                if(Character.isWhitespace(prueba[i])){
-                    System.out.println("hola");
-                    System.out.println(prueba[i]);
-                    System.out.println(string.charAt(prueba[i]));
-                    string.replace(prueba[i], 'x'); 
-               }
+            string = string.replaceAll("\\s+", "");
+            string = string.replaceAll("=", "");
+            char numero = string.charAt(0);
+
+            String formula = string;
+
+            //insert "1" in atom-atom boundry 
+            formula = formula.replaceAll("(?<=[A-Z])(?=[A-Z])|(?<=[a-z])(?=[A-Z])|(?<=\\D)$", "100");
+
+            //split at letter-digit or digit-letter boundry
+            String regex = "(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)";
+            String[] atoms = formula.split(regex);
+            for (String atom : atoms) {
+                System.out.println(atom);
             }
-            System.out.println(string);
             /*Hashtable<Integer, List> table = new Hashtable<Integer, List>();
             List l1 = new ArrayList<>();
             List l2 = new ArrayList<>();
