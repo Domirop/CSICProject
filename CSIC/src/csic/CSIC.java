4565f10eed40ec1ddb3ee30c60e48b3216914029
Domirop <domi.trivi2000@hotmail.com>
2020-05-05 15:01:36 +0100

    este si

--- CSIC/src/csic/CSIC.java
+++ CSIC/src/csic/CSIC.java
@@ -37,7 +37,9 @@ public static void main(String[] args) {
         } catch (IOException ex) {
             System.out.println(ex);
         }
-        /*String formula = string;
+        /*
+<-------------------------------SEPARAR NUMEROS Y LETRAS----------------------------------->        
+        String formula = string;
 
             insert "1" in atom-atom boundry 
             formula = formula.replaceAll("(?<=[A-Z])(?=[A-Z])|(?<=[a-z])(?=[A-Z])|(?<=\D)$", "100");
@@ -47,6 +49,40 @@ public static void main(String[] args) {
             String[] atoms = formula.split(regex);
             for (String atom : atoms) {
                 System.out.println(atom);
-            }*/
+            }
+<----------------------------MAPA-------------------------------->
+        Hashtable<Integer, List> table = new Hashtable<Integer, List>();
+            List l1 = new ArrayList<>();
+            List l2 = new ArrayList<>();
+            List l3 = new ArrayList<>();
+            List l4 = new ArrayList<>();
+            List l5 = new ArrayList<>();
+            l1.add("1");
+            l2.add("2");
+            l3.add("3");
+            l4.add("4");
+            l5.add("5");
+            table.put(1, l1);
+            table.put(2, l2);
+            table.put(3, l3);
+            table.put(4, l4);
+            table.put(5, l5);
+            
+            table = table.containsKey(5) ? adjuntar(table, 5, "eseeee") : anadir(table, 5, "holaa");
+            System.out.println(table.toString());
+            }
+            
+            public static Hashtable<Integer, List> anadir (Hashtable<Integer, List> table, int key, String value){
+            List nueva = new ArrayList<>();
+            nueva.add(value);
+            table.put(key, nueva);
+            return table;
+            }
+            public static Hashtable<Integer, List> adjuntar (Hashtable<Integer, List> table, int key, String value){
+            List nueva = table.get(key);
+            nueva.add(value);
+            table.put(key, nueva);
+            return table;
+        */
     }
 }
